de29cb3e4ce29500a29edb681dbd82984f7bc7ac
maxxxkam <muslimovmaxkam@gmail.com>
2024-09-24 18:54:40 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -165,3 +165,67 @@ const cards2 = ProductsData2.map(card => {
     `
 }).join('')
 wrapper2.innerHTML = cards2
+
+
+
+
+const modal = document.querySelector('.modal')
+const openModal = document.querySelector('#open-btn')
+const closeModal = document.querySelector('.close')
+
+openModal.onclick = () => {
+    modal.classList.add('active')
+}
+
+closeModal.onclick = () => {
+    modal.classList.remove('active')
+}
+
+
+document.getElementById('telegramForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+ 
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    const token = '6880452962:AAFtzPSvDWDuVRQ2KloHBt2Rv6yt4bauuUc';  // Замените на ваш токен
+    const chat_id = '-4595320926';  // Замените на ваш chat_id
+    const url = `https://api.telegram.org/bot${token}/sendMessage`;
+ 
+    // Формируем сообщение
+    const message = `Name: ${name}\nPhone: ${phone}`;
+ 
+    const data = {
+      chat_id: chat_id,
+      text: message
+    };
+ 
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+      if (data.ok) {
+        alert("Message sent!");
+        location.reload()
+      } else {
+        alert("Error sending message.");
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert("Error sending message.");
+    });
+  });
+
+
+  const theme = document.querySelector('#theme')
+  const body = document.querySelector('body')
+
+  theme .onclick = () => {
+    body.classList.toggle('dark')
+    
+  }
